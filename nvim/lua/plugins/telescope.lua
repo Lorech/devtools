d7722dd4688a1491b8c2946d8721baf9b9156bf7
Lorech <laurisdedumets@gmail.com>
2024-04-15 18:34:09 +0300

    Install Telescope for code actions

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,14 +1,30 @@
--- Configure Telescope file finder.
--- https://github.com/nvim-telescope/telescope.nvim
+-- Configure Telescope navigator.
+-- https://github.com/nvim-telescope/telescope.nvim for file finder
+-- https://github.com/nvim-telescope/telescope-ui-select.nvim for UI actions.
 return {
-  "nvim-telescope/telescope.nvim",
-  tag = "0.1.6",
-  dependencies = {
-    "nvim-lua/plenary.nvim"
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.6",
+    dependencies = {
+      "nvim-lua/plenary.nvim"
+    },
+    config = function()
+      local builtin = require("telescope.builtin")
+      vim.keymap.set("n", "<C-p>", builtin.find_files, {}) -- Ctrl + P to find files.
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep, {}) --  Space + F + G to grep files.
+    end
   },
-  config = function()
-    local builtin = require("telescope.builtin")
-    vim.keymap.set("n", "<C-p>", builtin.find_files, {}) -- Ctrl + P to find files.
-    vim.keymap.set("n", "<leader>fg", builtin.live_grep, {}) --  Space + F + G to grep files.
-  end
+  {
+    "nvim-telescope/telescope-ui-select.nvim",
+    config = function()
+      require("telescope").setup {
+      extensions = {
+        ["ui-select"] = {
+          require("telescope.themes").get_dropdown {}
+        }
+      }
+    }
+    require("telescope").load_extension("ui-select")
+    end
+  },
 }
