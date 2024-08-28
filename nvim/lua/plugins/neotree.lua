f891d034bb9b10ca0e858fdc0ea202cb291ce331
Lorech <laurisdedumets@gmail.com>
2024-08-28 23:10:13 +0300

    Display hidden files in Neotree and Telescope

--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -10,6 +10,22 @@ return {
     "3rd/image.nvim",
   },
   config = function()
+    require("neo-tree").setup({
+      window = {
+        position = "right",
+      },
+      filesystem = {
+        filtered_items = {
+          hide_dotfiles = false,
+          hide_gitignored = false,
+          hide_hidden = false,
+          never_show = {
+            ".git",
+          },
+        },
+      },
+    })
+
     vim.keymap.set("n", "<C-n>", ":Neotree filesystem toggle<CR>") -- Ctrl + N to toggle file directory.
   end,
 }
