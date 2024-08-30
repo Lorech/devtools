08c2358e053d2df52adcb1813a3736ab10d6a338
Lorech <laurisdedumets@gmail.com>
2024-08-30 11:30:11 +0300

    Update file finder keybind

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -10,7 +10,7 @@ return {
     },
     config = function()
       local builtin = require("telescope.builtin")
-      vim.keymap.set("n", "<C-p>", builtin.find_files, {})      -- Ctrl + P to find files.
+      vim.keymap.set("n", "<leader>ff", builtin.find_files, {}) --  Space + F + F to find files.
       vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})  --  Space + F + G to grep files.
     end,
   },
