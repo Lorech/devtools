74ed420901e52b918d82454c17c078475ad6e944
Lorech <laurisdedumets@gmail.com>
2024-09-04 21:28:57 +0300

    Switch to Rose Pine theme

---
+++ nvim/lua/plugins/colorschemes/rose-pine.lua
@@ -0,0 +1,6 @@
+-- Configuration for Rose Pine.
+-- https://github.com/rose-pine/nvim
+return {
+	"rose-pine/nvim",
+	name = "rose-pine",
+}
--- nvim/lua/plugins/ui/lualine.lua
+++ nvim/lua/plugins/ui/lualine.lua
@@ -4,7 +4,8 @@ return {
 	config = function()
 		require("lualine").setup({
 			options = {
-				theme = "catppuccin",
+        -- theme = "catppuccin",
+				theme = "rose-pine",
 			},
 		})
 	end,
