980ceab0577c6820781dd73cec6b7bb2bfa9c457
Lorech <laurisdedumets@gmail.com>
2024-09-16 09:57:20 +0300

    Add shortcut for viewing code usages

--- nvim/lua/plugins/development/lsp-config.lua
+++ nvim/lua/plugins/development/lsp-config.lua
@@ -33,6 +33,7 @@ return {
 
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {}) -- Open hovering documentation.
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {}) -- Open definition.
+			vim.keymap.set("n", "gr", vim.lsp.buf.references, {}) -- Open references/usages.
 			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {}) -- Open code actions.
 		end,
 	},
